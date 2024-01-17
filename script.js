d5fe2a1b6a86f28232c2bdedfaad84a8aef50da4
Zam <samuel.kodehode@gmail.com>
2024-01-18 00:42:37 +0100

    2.0.0

--- script.js
+++ script.js
@@ -1,18 +1,27 @@
-var factor1 = document.getElementById('input-1');
+var factor = document.getElementById('input-1');
+var maxFactor = 40;
 var button = document.getElementById('button');
 var sides = ['front', 'back', 'right', 'left', 'top', 'bottom'];
 var cube = document.querySelector('.cube');
 var calContainer = document.querySelector('.cal-container');
+var buttonRax = document.getElementById('rax');
+var buttonRay = document.getElementById('ray');
 button.addEventListener('click', function () {
-    var square = factor1.valueAsNumber * factor1.valueAsNumber;
-    renderCube(square, Math.sqrt(square), factor1.valueAsNumber);
+    if (factor.valueAsNumber > maxFactor) {
+        factor.valueAsNumber = 0;
+    }
+    var square = factor.valueAsNumber * factor.valueAsNumber;
+    renderCube(square, Math.sqrt(square), factor.valueAsNumber);
 });
-function renderCube(xYZ, columnsRows, calcNumber) {
+var renderCube = function (xYZ, columnsRows, factorX) {
     calContainer.innerHTML = ' ';
     cube.innerHTML = ' ';
+    factor.value = '';
     var img = document.createElement('img');
     var calculation = document.createElement('h4');
-    calculation.innerText = " ".concat(calcNumber, " x ").concat(calcNumber, " x ").concat(calcNumber, " = ").concat(xYZ * calcNumber, "  ");
+    factorX
+        ? calculation.innerText = " ".concat(factorX, " x ").concat(factorX, " x ").concat(factorX, " = ").concat(xYZ * factorX)
+        : error(calculation); //input control
     img.src = "img/cube-svgrepo-com%20(3).svg";
     img.id = 'logo';
     calContainer.appendChild(calculation);
@@ -27,9 +36,35 @@ function renderCube(xYZ, columnsRows, calcNumber) {
         for (var x = 0; x < xYZ; x++) {
             var tile = document.createElement('div');
             tile.className = 'tile';
-            tile.id = "tile".concat(x, "-").concat(sideNumber);
+            tile.id = "tile".concat(x, "-").concat(sideNumber); // ; is required ?
             tile.style.cssText = "--index: ".concat(x);
             cubeSides.appendChild(tile);
         }
     }
+};
+// cube rotation
+var xAxTrue = false;
+var yAxTrue = false;
+var xText = '-20deg';
+var yText = '1turn';
+buttonRax.addEventListener('click', function () {
+    xAxTrue = !xAxTrue;
+    xText = xAxTrue ? '1turn' : '-20deg';
+    updateKeyframes();
+});
+buttonRay.addEventListener('click', function () {
+    yAxTrue = !yAxTrue;
+    yText = yAxTrue ? '0' : '1turn';
+    updateKeyframes();
+});
+function updateKeyframes() {
+    document.styleSheets[0].deleteRule(0);
+    document.styleSheets[0].insertRule("\n        @keyframes cube-rotation {\n            from { transform: rotateX(-20deg) rotateY(0); }\n            to { transform: rotateX(".concat(xText, ") rotateY(").concat(yText, "); }\n        }\n    "), 0);
 }
+updateKeyframes();
+renderCube(1, 1, 1);
+//error handler
+var error = function (errorText) {
+    errorText.innerText = "ERROR only numbers 1-".concat(maxFactor);
+    errorText.style.color = 'rgb(255,5,5)';
+};
--- script.ts
+++ script.ts
@@ -1,50 +1,96 @@
+const factor: HTMLInputElement = document.getElementById('input-1')as HTMLInputElement
+const maxFactor: number = 40
+const button: HTMLElement = document.getElementById('button')
 
-const factor1 = document.getElementById('input-1')as HTMLInputElement
-const button = document.getElementById('button')
+const sides: string[] = ['front', 'back', 'right', 'left', 'top', 'bottom']
+const cube: Element = document.querySelector('.cube')
+const calContainer: Element = document.querySelector('.cal-container')
 
-const sides: string[] = ['front', 'back', 'right', 'left', 'top', 'bottom'];
-const cube = document.querySelector('.cube');
-const calContainer = document.querySelector('.cal-container')
+const buttonRax: HTMLElement = document.getElementById('rax')
+const buttonRay: HTMLElement = document.getElementById('ray')
 
-button.addEventListener('click', () => {
-    let square = factor1.valueAsNumber * factor1.valueAsNumber
-    renderCube(square, Math.sqrt(square), factor1.valueAsNumber )
+button.addEventListener('click', (): void => {
+    if (factor.valueAsNumber > maxFactor) {
+        factor.valueAsNumber = 0
+    }
+    const square: number = factor.valueAsNumber * factor.valueAsNumber
+    renderCube(square, Math.sqrt(square), factor.valueAsNumber )
 })
 
-function renderCube (xYZ: number, columnsRows: number, calcNumber){
+const renderCube = (xYZ: number, columnsRows: number, factorX: number):void =>{
     calContainer.innerHTML = ' '
     cube.innerHTML = ' '
+    factor.value = ''
 
-    const img = document.createElement('img')
-    const calculation = document.createElement('h4')
+    const img: HTMLImageElement = document.createElement('img')
+    const calculation: HTMLHeadingElement = document.createElement('h4')
 
-    calculation.innerText = ` ${calcNumber} x ${calcNumber} x ${calcNumber} = ${xYZ * calcNumber}  `
+    factorX
+        ? calculation.innerText = ` ${factorX} x ${factorX} x ${factorX} = ${xYZ * factorX}`
+        : error(calculation) //input control
+
     img.src ="img/cube-svgrepo-com%20(3).svg"
     img.id = 'logo'
 
     calContainer.appendChild(calculation)
     calContainer.appendChild(img)
 
-    for (let sideNumber = 0; sideNumber < sides.length; sideNumber++) {
-        const cubeSides = document.createElement('div');
+    for (let sideNumber: number = 0; sideNumber < sides.length; sideNumber++) {
+        const cubeSides: HTMLDivElement = document.createElement('div')
 
-        cubeSides.className = `side ${sides[sideNumber]}`;
+        cubeSides.className = `side ${sides[sideNumber]}`
         cubeSides.style.gridTemplateColumns = `repeat(${columnsRows}, 1fr)`
         cubeSides.style.gridTemplateRows = `repeat(${columnsRows}, 1fr)`
-        cubeSides.id = `sides${sideNumber}`;
-        cube.appendChild(cubeSides);
+        cubeSides.id = `sides${sideNumber}`
+        cube.appendChild(cubeSides)
 
-        for (let x = 0; x < xYZ; x++) {
-            const tile = document.createElement('div');
+        for (let x: number = 0; x < xYZ; x++) {
+            const tile: HTMLDivElement = document.createElement('div')
 
-            tile.className = 'tile';
-            tile.id = `tile${x}-${sideNumber}`;
-            (tile as HTMLElement).style.cssText = `--index: ${x}`;
-            cubeSides.appendChild(tile);
+            tile.className = 'tile'
+            tile.id = `tile${x}-${sideNumber}`; // ; is required ?
+            (tile as HTMLElement).style.cssText = `--index: ${x}`
+            cubeSides.appendChild(tile)
         }
     }
+
+}
+// cube rotation
+let xAxTrue: boolean = false
+let yAxTrue: boolean = false
+
+let xText: string = '-20deg'
+let yText: string = '1turn'
+
+buttonRax.addEventListener('click', ():void => {
+    xAxTrue = !xAxTrue
+    xText = xAxTrue ? '1turn' : '-20deg'
+
+    updateKeyframes()
+})
+
+buttonRay.addEventListener('click', ():void => {
+    yAxTrue = !yAxTrue
+    yText = yAxTrue ? '0' : '1turn'
+
+    updateKeyframes()
+});
+
+function updateKeyframes() {
+    document.styleSheets[0].deleteRule(0)
+    document.styleSheets[0].insertRule(`
+        @keyframes cube-rotation {
+            from { transform: rotateX(-20deg) rotateY(0); }
+            to { transform: rotateX(${xText}) rotateY(${yText}); }
+        }
+    `, 0)
 }
 
+updateKeyframes()
 
-
-
+renderCube(1,1,1)
+//error handler
+const error = (errorText: HTMLHeadingElement): void => {
+    errorText.innerText = `ERROR only numbers 1-${maxFactor}`
+    errorText.style.color = 'rgb(255,5,5)'
+}
